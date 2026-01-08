1c8f4463c1c4362183ae164e334cdc36e0181045
Delta Student <pappu41524@gmail.com>
2026-01-08 15:27:09 +0530

    logging and exception

--- setup.py
+++ setup.py
@@ -1,26 +1,3 @@
-# from setuptools import setup, find_packages
-# from typing import List
-
-# HYPHEN_E_DOT = '-e .'
-# def get_requirements(file_path:str)->List[str]:
-#     ''''
-#     this function will return the list of requirements
-#     '''
-#     requirements=[]
-#     with open(file_path) as file_obj:
-#         requirements=file_obj.readlines()
-#         requirements=[req.replace("\n","") for req in requirements]
-#         if HYPEN-E_DOT in requirements:
-#             requirements.remove(HYPEN-E_DOT)
-#     return requirements
-# setup(
-#     name='mlproject',
-#     version='0.1.0',
-#     author='Pappu',
-#     author_email='pappu41524@gmail.com',
-#     packages=find_packages(),
-#     install_requires=get_requirements('requirements.txt')
-# )
 from setuptools import setup, find_packages
 from typing import List
 
